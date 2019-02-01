286a9bc88091988bbd2429b589c0cca667614dfa
Rick <38975677+RickMa1104@users.noreply.github.com>
2019-02-01 17:46:50 +0800

    Update card.php

--- card.php
+++ card.php
@@ -1,13 +1,12 @@
 <?php
 	$cid=$_GET["cid"];
-	$mysqli=new mysqli("localhost","root","12345678","card");
-	$result =$mysqli->query("seletc id,title from card where cid={$cid}");
-
-	while($row=$result->fetch_assoc()){
+	$mysqli=new mysqli("localhost","root","Ma123456","card");
+	$result =$mysqli->query("select id,title from card where cid=$cid");
+	$mysqli->close();
+	
+	echo "<br/>";
+	while($row=$result->fetch_array(MYSQLI_ASSOC)){
 		echo '<ul>';
 		echo '<li> <a href="con.php?id='.$row["id"].'">'.$row["title"].'</a></li>';
-
-		echo '</ul>';
-	
-	
+		echo '</ul>'; 
 	}
